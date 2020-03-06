bacd740c6ff1089451434c5dbd8f41385e010dc3
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-06 14:37:04 -0500

    Initial commit

---
+++ Hockey_goals/TT_Hockey_Goals.R
@@ -0,0 +1,45 @@
+##############################
+## Tidy Tuesday Hockey Goals
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 2020-03-06
+##
+##############################
+
+dir.create('~/PhD/GitHub/TidyTuesday')
+dir.create('~/PhD/GitHub/TidyTuesday/Hockey_goals')
+
+setwd('~/PhD/GitHub/TidyTuesday/Hockey_goals')
+
+
+library(patchwork)
+library(janitor)
+library(devtools)
+library(skimr)
+library(rsed)
+library(data.table)
+library(sjPlot)
+library(tidyverse)
+
+theme_set(theme_bw())
+
+## Read in data
+# game_goals = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/game_goals.csv')
+# top_250 = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/top_250.csv')
+season_goals = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/season_goals.csv')
+
+season_goals %>% 
+arrange(penalty_min) %>%
+  select(penalty_min) %>% 
+  View()
+
+ggplot(data = season_goals,
+       aes(x = yr_start, 
+           y = penalty_min)) +
+  geom_hex() +
+  labs(x = 'Year season started', 
+       y = 'Penalty minutes') +
+  theme(panel.grid = element_blank(), 
+        axis.title = element_text(size = 15), 
+        axis.text = element_text(size = 15))
