f68e20157ed668b75b077dec85fd45dc2da28fe9
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-13 12:04:44 -0400

    data

--- College_tuition/College_tutition.R
+++ College_tuition/College_tutition.R
@@ -22,3 +22,15 @@ library(tidyverse)
 theme_set(theme_bw())
 
 ## Read in the data
+
+tuition_cost = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_cost.csv')
+
+tuition_income = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
+
+salary_potential = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv')
+
+historical_tuition = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/historical_tuition.csv')
+
+diversity_school = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/diversity_school.csv')
+
+
