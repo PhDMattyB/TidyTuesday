d23a8d0925a9e0f839e490d644ecff125dbf5d90
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-13 14:04:43 -0400

    made the map

--- College_tuition/College_tutition.R
+++ College_tuition/College_tutition.R
@@ -46,7 +46,33 @@ state_cost = tuition_cost %>%
   summarise(avg_in_tuition = mean(in_state_total),
             avg_out_tuition = mean(out_of_state_total))
 
-ggplot()
+map_states = map_data('state') %>% 
+  as_tibble() %>% 
+  rename(state = region) %>% 
+  select(-order)
+
+## state names in the map data is lower case, 
+## change the state names in the dataframe to lower case
+state_cost$state = str_to_lower(sate_cost$state, locale = 'en')
+
+plot_date = inner_join(state_cost, 
+                       map_states) %>% 
+  distinct() 
+
+ggplot(data = plot_date) +
+  geom_polygon(aes(x = long, 
+                   y = lat, group = state, 
+                   fill = avg_in_tuition), 
+               col = 'black')
+
+ggplot() + 
+  geom_polygon( aes(x=long, y=lat, group=group, fill=percentage),
+                color="black" )+
+  scale_fill_gradient2(name= "%",low="white",mid="#A770CD", high="#410066", midpoint = 55)+
+  labs(title= "Women in the US colleges", 
+       subtitle="At least there is 50% of women \n except in North Dakota (49.2%)")+
+  theme_ari()+theme(plot.title = element_text(family=font,face = "bold",
+                                              colour = "#410066", size = 18,  hjust = 0.5))
 
 # tuition cost and career pay ---------------------------------------------
 
