3ed14866fd02123034cf0bd3d603309b75b7f96a
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-13 12:09:41 -0400

    Update College_tutition.R

--- College_tuition/College_tutition.R
+++ College_tuition/College_tutition.R
@@ -27,10 +27,18 @@ tuition_cost = read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 
 tuition_income = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
 
-salary_potential = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv')
+salary_potential = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv') %>% 
+  rename(state = state_name)
 
 historical_tuition = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/historical_tuition.csv')
 
 diversity_school = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/diversity_school.csv')
 
 
+Tuition_diversity = left_join(tuition_cost, 
+                               diversity_school, 
+                               by = c('name', 'state'))
+
+large_data = left_join(Tuition_diversity, 
+                 salary_potential, 
+                 by = c('name', 'state'))
