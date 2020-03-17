26ad591fd4815e6eb8acd195867327d7e3a8e7eb
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-17 13:43:43 -0400

    Update Office_Rating.R

--- Office/Office_Rating.R
+++ Office/Office_Rating.R
@@ -23,23 +23,37 @@ split = str_split_fixed(data$air_date, '-', n = 3) %>%
 
 data_fixed = bind_cols(data, split)
 
+pal = c('#9082FF',
+        '#FFCC8F', 
+        '#759CFF', 
+        '#FFE85C',
+        '#29BAFF',
+        '#E67CFF',
+        '#29FFAD',
+        '#759CFF',
+        '#BBFF5C')
+
 data_fixed %>% 
   group_by(season) %>% 
   ggplot()+
   geom_point(aes(x = season, 
                  y = imdb_rating, 
-                 col = season),
+                 col = as.factor(season)),
              size = 2)+
   geom_smooth(aes(x = season, 
                   y = imdb_rating), 
               method = 'loess', 
-              col = 'black', 
+              col = 'white', 
               size = 2)+
   labs(x = 'Season', 
        y = 'IMDB Rating')+
-  theme_classic()+
+  scale_color_manual(values = pal)+
+  # theme_classic()+
+  theme_dark()+
   scale_x_continuous(breaks = seq(1, 9, by = 1))+
   theme(axis.title = element_text(size = 14), 
         axis.text = element_text(size = 12), 
         axis.ticks = element_line(size = 1), 
-        axis.line = element_line(size = 1))
+        axis.line = element_line(size = 1),
+        legend.position = 'none',
+        panel.grid = element_blank())
