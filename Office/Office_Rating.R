646073daac5c1cdb8f21c9a0b0cfd7149eb1131e
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-17 13:25:29 -0400

    Update Office_Rating.R

--- Office/Office_Rating.R
+++ Office/Office_Rating.R
@@ -15,3 +15,24 @@ library(tidyverse)
 
 data = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
 
+split = str_split_fixed(data$air_date, '-', n = 3) %>% 
+  as_tibble() %>% 
+  rename(year = V1, 
+         Month = V2, 
+         Day = V3)
+
+data_fixed = bind_cols(data, split)
+
+data_fixed %>% 
+  group_by(season) %>% 
+  ggplot()+
+  geom_point(aes(x = season, 
+                 y = imdb_rating, 
+                 col = season))+
+  geom_smooth(aes(x = season, 
+                  y = imdb_rating), 
+              method = 'loess', 
+              col = 'black')+
+  labs(x = 'Season', 
+       y = 'IMDB Rating')+
+  theme_classic()
