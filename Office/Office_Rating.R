22ae6a55b71b9fd182fcb3949c80349c95261e48
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2020-03-17 13:12:12 -0400

    Update Office_Rating.R

--- Office/Office_Rating.R
+++ Office/Office_Rating.R
@@ -13,3 +13,5 @@ setwd('~/PhD/GitHub/TidyTuesday/Office/')
 library(patchwork)
 library(tidyverse)
 
+data = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
+
