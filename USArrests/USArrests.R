1acc2d7fc0ede65c230b3812f1941e3c1ebbfcc8
Brachmann <Matthew.Brachmann@dfo-mpo.gc.ca>
2021-06-04 17:02:22 -0300

    Update USArrests.R

--- USArrests/USArrests.R
+++ USArrests/USArrests.R
@@ -11,32 +11,24 @@
 library(tidyverse)
 library(maps)
 library(scatterpie)
-library(janitor)
 
-# reproducible example ----------------------------------------------------
-# quakes
-# 
-# quakes %>% 
-#   as_tibble() %>% 
-#   arrange(long)
-# 
-# 
-# Japan = map_data('world') %>% 
-#   filter(region == 'Japan') %>% 
-#   as_tibble()
-
+## Load the data and clean it
 data('USArrests')
+
+## splitting out the states because that info will disappear when
+## the data is converted to a tibble
 states = USArrests %>% 
   rownames() %>% 
   as_tibble()
 
+## Add the information back in
 clean_data = bind_cols(USArrests, 
                        states) %>% 
   as_tibble() %>% 
   rename(State = value)%>% 
-  mutate(State = tolower(State))
+  mutate(State = tolower(State)) ## lowercase matches map data
 
-
+## calculate mean lat and longs for each state
 mean_map_data = map_data('state') %>% 
   as_tibble() %>% 
   group_by(region) %>% 
@@ -44,20 +36,25 @@ mean_map_data = map_data('state') %>%
             mean_long = mean(long)) %>% 
   rename(State = region)
 
+## full data set
 clean_data = inner_join(clean_data, 
           mean_map_data) %>% 
   select(-UrbanPop) %>% 
   group_by(State)
 
+## map of the US
 us_map_data = map_data('state') %>% 
   as_tibble()
 
+## remove all axes
 theme_set(theme_void())
 
+## colour palette
 col_pal = c('#034C8C', 
             '#04BF7B', 
             '#D7F205')
 
+## Actual plot
 ggplot(us_map_data)+
   geom_map(data = us_map_data, 
            map = us_map_data, 
@@ -68,7 +65,7 @@ ggplot(us_map_data)+
            fill = '#9BA7BF')+
   labs(x = 'Longitude', 
        y = 'Latitude', 
-       fill = 'Arrested')+
+       fill = 'Crime')+
   scale_fill_manual(values = col_pal)+
   geom_scatterpie(data = clean_data, 
                   aes(x = mean_long, 
@@ -76,4 +73,8 @@ ggplot(us_map_data)+
                       group = State), 
                   pie_scale = 1, 
                   cols = colnames(clean_data[,c(1:3)]))+
-  coord_fixed()
+  coord_fixed()+
+  theme(legend.position = 'bottom', 
+        legend.title = element_text(size = 12, 
+                                    face = 'bold'), 
+        legend.text = element_text(size = 10))
